5bab968afefed462da048b5224420ff93875bf6c
antoninpaoli <53855385+antoninpaoli@users.noreply.github.com>
2020-03-27 23:57:18 +0100

    Version finale

--- starting_kit/sample_code_submission/preprocess.py
+++ starting_kit/sample_code_submission/preprocess.py
@@ -1,13 +1,37 @@
+<<<<<<< HEAD
+from sklearn.ensemble import IsolationForest
+from sklearn.feature_selection import VarianceThreshold
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+=======
 from data_manager import DataManager
 from data_io import write
 from sklearn.ensemble import IsolationForest
 from sklearn.preprocessing import StandardScaler
 from sklearn.feature_selection import VarianceThreshold
 from sklearn.decomposition import PCA
+>>>>>>> 265004997adba0f1a9c6a2a865d2aa4aab7aa7fb
 
 class preprocess :
     def __init__(self):
         self.reg = IsolationForest(max_samples=100)
+<<<<<<< HEAD
+
+    def fit_transform (self, X) :
+        #Outliers Detection
+        self.reg.fit(X)
+        self.reg.predict(X)
+        
+        #Feature selection
+        sel = VarianceThreshold(threshold=(.7 * (1 - .7)))
+        X_train= sel.fit_transform(X)
+
+        #scaling
+        scaler = StandardScaler()
+        scaled_X = scaler.fit_transform(X_train)
+
+        return scaled_X
+=======
         self.scaler = StandardScaler()
     
     def fit_transform(self, X):
@@ -29,3 +53,4 @@ class preprocess :
         print(pca.explained_variance_ratio_)
         print(pca.singular_values_)
         return N_X
+>>>>>>> 265004997adba0f1a9c6a2a865d2aa4aab7aa7fb
